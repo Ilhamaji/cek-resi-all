bb72aef2e8b483a10395fe6a6ee15d5563fc4809
ilhamaji <iheadskull@gmail.com>
2023-10-12 02:09:07 +0700

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="*" element={<Notfound />} />
-          <Route path="cek-resi-all" element={<Notfound />} />
+          <Route path="cek-resi-all" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="jne" element={<Jne />} />
